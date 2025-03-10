f6375ebe41a81f3ef1f4d849c77e05210aa9f19e
huzaifa-jawad367 <hjawad.bscs21seecs@seecs.edu.pk>
2025-03-11 00:31:06 +0500

    Added the splash screen view in SwiftUIView and Storyboard

--- AI_FORECAST_App/Views/SplashScreenView.swift
+++ AI_FORECAST_App/Views/SplashScreenView.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 struct SplashScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // MARK: - Background Gradient
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color.topBlue,  // Top color
+                    Color.bottomBlue                // Bottom color
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            // MARK: - Logo & Text
+            VStack(spacing: 10) {
+                // Replace with an Image if you have a custom logo asset
+                Text("AI-ForCaST")
+                    .font(.system(size: 36, weight: .bold))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                Text("Revolutionizing Forest \nSurveys with AR Precision")
+                    .font(.system(size: 18, weight: .medium))
+                    .foregroundColor(.white.opacity(0.9))
+                    .multilineTextAlignment(.center)
+            }
+        }
     }
+
 }
+
+extension Color {
+    static let topBlue = Color(red: 106/255, green: 150/255, blue: 219/255)  // #6b96db
+    static let bottomBlue = Color(red: 41/255, green: 83/255, blue: 156/255) // #29539c
+}
+
 
 #Preview {
     SplashScreenView()
