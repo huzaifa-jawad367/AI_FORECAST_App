db0449487d66dad0c7de2f6b4147eb06fee4be27
huzaifa-jawad367 <hjawad.bscs21seecs@seecs.edu.pk>
2025-03-11 00:55:47 +0500

    Navigating to project lists through Nav view

--- AI_FORECAST_App/Views/ProjectListView.swift
+++ AI_FORECAST_App/Views/ProjectListView.swift
@@ -12,42 +12,48 @@ struct ProjectListView: View {
     @Binding var authState: AuthState
     
     var body: some View {
-        NavigationView {
-            ZStack(alignment: .bottomTrailing) {
-                NavigationLink {
-                    ScansListView(authState: .constant(.ScansList))
-                } label: {
-                    // Using a List so pull-to-refresh works by default on iOS 16+
-                    List {
-                        ForEach(viewModel.projects) { project in
-                            ProjectCardView(project: project)
-                                .listRowSeparator(.hidden) // Remove default row style
-                                .listRowInsets(EdgeInsets())
-                        }
-                    }
-                    .listStyle(.plain)
-                    .navigationTitle("Projects")
-                    .refreshable {
-                        await viewModel.fetchScans()
-                    }
-                    .task {
-                        await viewModel.fetchScans()
+        
+//        NavigationView {
+            
+        ZStack(alignment: .bottomTrailing) {
+            
+            NavigationLink {
+                ScansListView(authState: .constant(.ScansList))
+            } label: {
+                // Using a List so pull-to-refresh works by default on iOS 16+
+                List {
+                    ForEach(viewModel.projects) { project in
+                        ProjectCardView(project: project)
+                            .listRowSeparator(.hidden) // Remove default row style
+                            .listRowInsets(EdgeInsets())
                     }
                 }
-                
-                // Floating circular button at the bottom right
-                NavigationLink(destination: CreateProjectView()) {
-                    Image(systemName: "plus")
-                        .font(.system(size: 36))
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .clipShape(Circle())
-                        .shadow(radius: 4)
+                .listStyle(.plain)
+                .navigationTitle("Projects")
+                .refreshable {
+                    await viewModel.fetchScans()
                 }
-                .padding(50)
+                .task {
+                    await viewModel.fetchScans()
+                }
             }
+            
+            // Floating circular button at the bottom right
+            NavigationLink(destination: CreateProjectView()) {
+                Image(systemName: "plus")
+                    .font(.system(size: 36))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                    .shadow(radius: 4)
+            }
+            .padding(50)
         }
+//        }
+    
+        
+        
     }
 }
 
